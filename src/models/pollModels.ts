71ac5a2d8f37de711b86e2fea812ab876cecb09e
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2022-10-23 19:19:50 +1100

    cookies are slightly problematic as expected :/

--- src/models/pollModels.ts
+++ src/models/pollModels.ts
@@ -14,9 +14,4 @@ export interface PollSubmission {
     }
 }
 
-const e = {
-    "identification":
-        { "poll": { "id": 1, "name": "poll 1", "description": "poll 1 description", "options": ["Option 1", "Option 2", "Option 3"] }, "choice": 4, "discord": null }
-}
-
 export const discordOauthUrl: string = 'https://discord.com/api/oauth2/authorize?client_id=887832942804619304&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fpolls%3FoauthSuccess%3Dtrue&response_type=code&scope=identify'
--- src/routes/polls.ts
+++ src/routes/polls.ts
@@ -16,13 +16,13 @@ const polls: PollQuestion[] = [{
     id: 1,
     name: 'poll 1',
     description: 'poll 1 description',
-    options: ["Option 1", "Option 2", "Option 3"]
+    options: ["Option 1", "Option 2", "Option 3", "Option 4"]
 },
 {
     id: 2,
     name: 'poll 2',
     description: 'poll 2 description',
-    options: ["Option 1", "Option 2", "Option 3"]
+    options: ["Option 1", "Option 2", "Option 3", "Option 4"]
 }];
 
 // middleware that is specific to this router
@@ -39,7 +39,7 @@ app.get('/:id', async (req: Request, res: Response) => {
     }
     const choice = parseInt(String(req.query['choice'])) || NaN;
     if (req.query['discord']) { // if the choice is contained in the url (the person probably sent it from discord)- this does not require frontend.
-        res.cookie('identification', { poll: poll, choice: choice ?? null, discord: null }) // save the option in a cookie
+        res.cookie('rememberChoice', { poll: poll, choice: choice ?? null, discord: undefined }, { maxAge: 1000 * 60 * 5, httpOnly: false }) // save the option in a cookie
         console.debug('redirected')
         return res.redirect(discordOauthUrl)
     }
@@ -47,15 +47,14 @@ app.get('/:id', async (req: Request, res: Response) => {
     console.debug('file sent')
 })
 
-app.get('/', async (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
     if (req.query['oauthSuccess']) {
-        if (!req.cookies.identification) return res.status(400).send('No identification cookie found. Did you come back from the <a href="">Discord OAuth prompt?</a>')
-        let choice = req.cookies.identification.choice
-        let poll = req.cookies.identification.poll
-        const user = await discordOauth(req)
-        console.log('usr: ', user)
-        res.cookie('identification', { poll: poll, choice: this ?? null, discord: user })
-        if (choice > poll.options.length || isNaN(choice)) return res.status(400).send('Invalid choice');
+        if (!req.cookies.rememberChoice) return res.status(400).send('No identification cookie found. Did you come back from the <a href="">Discord OAuth prompt?</a>')
+        let choice: number = parseInt(req.cookies.rememberChoice.choice) || NaN;
+        let poll = req.cookies.rememberChoice.poll
+        const user = discordOauth(req)
+        res.cookie('identification', { poll: poll, choice: choice, discord: user }, { maxAge: 1000 * 60 * 5 })
+        if (choice > poll.options.length || isNaN(choice)) return res.status(400).send(`Invalid choice. ${choice}`);
         // create submit code
         submission = {
             choice: choice,
@@ -65,7 +64,8 @@ app.get('/', async (req: Request, res: Response) => {
                 discord_id: req.cookies.identification.discord.id
             }
         }
-        return fetch(`${req.url}/polls/${poll.id}`, {
+        res.send(submission)
+        return fetch(`${req.protocol + '://' + req.get('host')}/polls/${poll.id}`, {
             method: 'POST',
             body: JSON.stringify(submission),
             headers: { 'Content-Type': 'application/json' },
@@ -77,7 +77,6 @@ app.get('/', async (req: Request, res: Response) => {
 
 app.get('/:id/json', async (req: Request, res: Response) => {
     const user = await discordOauth(req)
-    console.log(user)
     console.log(req.query)
     const poll = polls.find(s => s.id === parseInt(req.params.id));
     if (!poll) res.status(404).send('The poll with the given ID was not found.');
@@ -85,13 +84,14 @@ app.get('/:id/json', async (req: Request, res: Response) => {
 })
 
 app.post("/:id", (req: Request, res: Response) => {
-    submission = {
-        choice: parseInt(req.body['option'] ?? req.body['choice']),
-        who: {
-            username: req.body['username'] || undefined,
+    if (req.body['option']) { //frontend response
+        submission = {
+            choice: parseInt(req.body['option'] ?? req.body['choice']),
+            who: {
+                username: req.body['username'] || undefined,
+            }
         }
-    }
-    console.log(submission)
+    } else submission = req.body //rpc response
     return res.send(`thank youu for responding to my poll ${submission.who?.username ?? ''}! u submitted <b>option ${req.body["option"]}</b> as your option.`)
 });
 
